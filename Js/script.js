5bef0835ea731805fd7832d0d0e25731e9fc8d87
Deddkal <m.araolaza89@gmail.com>
2022-11-20 03:01:02 -0300

    back-up trabajos de facundo y franco

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+const userName = document.getElementById("userName")
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+const cuerpoModal = document.getElementById('cuerpoModal')
+
+myModal.addEventListener('click', (e)=>{
+    e.preventDefault()
+    datosCliente(userName)
+})
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+function datosCliente(inputDelNombre){
+    cuerpoModal.innerHTML = `<p> Hola ${inputDelNombre.value}, gracias por enviarnos tu informacion. Pronto nos contactaremos con ud! :D</p>`
+}
--- jugando.js
+++ jugando.js
@@ -5,6 +5,7 @@ const app = createApp ({
         return{
             productos: [],
             carrito: [],
+            consumoTotal: 0,
         }
     },
     created(){
@@ -15,7 +16,8 @@ const app = createApp ({
             this.controlCarrito()
             this.productos=json.response
             this.productos.map(e => this.nuevaPropiedad(e))
-            console.log(this.productos)
+            this.calcularCompraCarrito()
+            
             
         })
         .catch(error => console.log(error))
@@ -30,6 +32,7 @@ const app = createApp ({
             aux = this.productos.find( element => element._id == e.target.id)
             console.log(aux._id)
             if ((this.carrito.length == 0) || !(this.carrito.find( e => e._id == aux._id ))) {
+                aux.cantidad=1
                 this.carrito.push(aux)
                 localStorage.setItem('carrito', JSON.stringify(this.carrito))
             }
@@ -42,16 +45,44 @@ const app = createApp ({
             }
         },
         sumarProducto(e){
-            let aux = e.target.id.slice(5)
-            let productoSeleccionado = this.productos.find(elemento => elemento._id == aux)
-            console.log(productoSeleccionado)
+            let aux = e.target.id.slice(5)            
+            if((this.carrito.find(elemento => elemento._id == aux))){
+                this.carrito.filter(e => aux == e._id).map(e => {if(e.cantidad<e.stock){e.cantidad++}})
+            }            
         },
-        restarProducto(){
+        restarProducto(e){
+            let aux = e.target.id.slice(6)
+            if((this.carrito.find(elemento => elemento._id == aux))){
+                this.carrito.filter(e => aux == e._id).map(e => {if(e.cantidad>1){e.cantidad--}})
+            }
+        },
+        finalizarCompra(e){
+            e.preventDefault()
+            alert(`Compra finalizada el monto a abonar es de ${this.consumoTotal}`)
+            this.borraCarrito(e)
 
+        },
+        borraCarrito(e){
+            e.preventDefault()
+            localStorage.setItem('carrito', JSON.stringify([]))
+            this.carrito = []
+        },
+        calcularCompraCarrito(){
+            if (this.carrito.length!=0){
+                this.consumoTotal = this.carrito.map(e => e.precio*e.cantidad).reduce((a,b) => a+=b)
+            } else{
+                this.consumoTotal= 0
+            }
         }
     },
     computed:{
-
+        calcularCompra(){
+            if(this.carrito.length!=0){
+            let aux = '$' + this.carrito.map(e => e.precio*e.cantidad).reduce((a,b) => a+=b)
+            console.log(aux)
+           this.consumoTotal = aux
+            }else{this.consumoTotal = '$' + 0}
+        }
     }
 })
 
